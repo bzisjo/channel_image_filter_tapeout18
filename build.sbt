a1023fa742be60c10674801041e7b51c7436e694
Jonathan Wang <bzisjo@gmail.com>
2018-03-22 20:38:33 -0700

    fir

--- build.sbt
+++ build.sbt
@@ -39,10 +39,10 @@ resolvers ++= Seq(
 
 // Provide a managed dependency on X if -DXVersion="" is supplied on the command line.
 val defaultVersions = Map(
-  "chisel3" -> "3.1-SNAPSHOT",
-  "chisel-iotesters" -> "1.2-SNAPSHOT",
-  "dsptools" -> "1.1-SNAPSHOT"
-)
+  "chisel3" -> "3.2-SNAPSHOT",
+  "chisel-iotesters" -> "1.3-SNAPSHOT",
+  "dsptools" -> "1.2-SNAPSHOT"
+  )
 
 libraryDependencies ++= (Seq("chisel3","chisel-iotesters","dsptools").map {
   dep: String => "edu.berkeley.cs" %% dep % sys.props.getOrElse(dep + "Version", defaultVersions(dep)) })
--- src/main/scala/utils/FIRfilter.scala
+++ src/main/scala/utils/FIRfilter.scala
@@ -2,3 +2,26 @@ import chisel3._
 import chisel3._
 import chisel3.util._
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+
+class FIRfilter(length: Int, bitwidth: Int, import_coeffs: Seq[Int]) extends Module{
+	val io = IO(new Bundle {
+		val in = Input(SInt(bitwidth.W))
+		val out = Output(SInt(bitwidth*2+length-1).W))	// figure out bit growth later
+	})
+
+
+	val coeffs = import_coeffs.map(_.S)
+
+	val delays = Seq.fill(length)(Wire(UInt(bitwidth.W))).scan(io.in)(prev: SInt, next: SInt) => {
+		next := RegNext(prev)
+		next
+	}
+
+	val mults = delays.zip(coeffs).map { case(delay: UInt, coeff: UInt) => delay * coeff }
+
+	val result = mults.reduce(_+&_)
+
+	io.out := result
+
+}
